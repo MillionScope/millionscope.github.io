54692ce6e6bbd2e720ba31f4b0fbaa8a99ab0234
hmthanh <hmthanhgm@gmail.com>
2023-06-03 22:41:49 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 // const rehypeKatex = require("rehype-katex");
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
+  output: 'export',
   latex: true,
   i18n: {
     locales: ["en-US", "vi-VI"],
