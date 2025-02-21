ef2068af1711e1527c61b3174ba9147723184546
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-21 22:56:12 +0700

    update

---
+++ config/constants.js
@@ -0,0 +1,9 @@
+export const DEFAULT_THEME = {
+  darkMode: true,
+  direction: "ltr",
+}
+
+
+export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT
+export const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+export const GITHUB_REDIRECT_URI = `${API_ENDPOINT}/auth/callback/github`
