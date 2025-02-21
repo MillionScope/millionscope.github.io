e4df01fb432fc867aaf46769f876d195bcd36c55
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-22 00:51:24 +0700

    remove log

--- config/constants.js
+++ config/constants.js
@@ -6,7 +6,3 @@ export const DEFAULT_THEME = {
 export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT
 export const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
 export const GITHUB_REDIRECT_URI = `${API_ENDPOINT}/auth/callback/github`
-
-console.log("GITHUB_CLIENT_ID", GITHUB_CLIENT_ID)
-console.log("GITHUB_REDIRECT_URI", GITHUB_REDIRECT_URI)
-console.log("API_ENDPOINT", API_ENDPOINT)
