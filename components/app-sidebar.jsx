e78eb60a57dba85c85a55e63e33646f0054c2820
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-23 16:15:05 +0700

    redefine css MillionScope

--- components/app-sidebar.jsx
+++ components/app-sidebar.jsx
@@ -27,7 +27,7 @@ export function AppSidebar() {
               }}
               className="flex flex-row gap-3 items-center"
             >
-              <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">Chatbot</span>
+              <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">MillionScope</span>
             </Link>
             <Tooltip>
               <TooltipTrigger asChild>
