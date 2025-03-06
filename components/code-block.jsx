8e1ea36ac6f77d7718716b423d90fd2a15da8b10
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-06 10:34:57 +0700

    fix code block

--- components/code-block.jsx
+++ components/code-block.jsx
@@ -4,11 +4,11 @@ export function CodeBlock({ node, inline, className, children, ...props }) {
   const match = /language-(\w+)/.exec(className || "")
   if (!inline) {
     return match ? (
-      <pre className="text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900">
+      <pre className="">
         <code className={`whitespace-pre-wrap break-words language-${match[1]}`}>{children}</code>
       </pre>
     ) : (
-      <code className="whitespace-pre-wrap break-words px-1 py-0.5 bg-gray-200 dark:bg-gray-800 rounded-md">{children}</code>
+      <code className="whitespace-pre-wrap break-words px-1 py-0.5 dark:bg-gray-800 rounded-md">{children}</code>
     )
   } else {
     return (
